360aed94dc5c6607dcd72dd7e64daedac8945bce
shubhamm45 <shubhamm45@yahoo.com>
2021-08-04 08:27:38 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.60.0"
-    }
-  }
-}
 provider "azurerm" {
   features {}
    alias ="west"
