dff95fe42f1d7fe1e7e1f79ce5bb77531c573419
shubhamm45 <shubhamm45@yahoo.com>
2021-08-04 08:32:34 +0000

    Added vnet module

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.60.0"
+      version = "=3.5.0"
     }
   }
 }
@@ -10,7 +10,7 @@ terraform {
 provider "azurerm" {
   features {}
   alias ="west1"
-   version = "~> 1.44"
+   version = "=3.5.0"
 }
 
 resource "azurerm_resource_group" "myresourcegroup" {
